bb84d7673f12c3bb5dc3dab8967479e9ccaf5768
andthen36 <meyer.evan90@gmail.com>
2023-02-01 11:54:41 -0500

    push

--- EMU Winter 2023/COSC 341/C/program.c
+++ EMU Winter 2023/COSC 341/C/program.c
@@ -24,6 +24,7 @@ int compute_pi(int n){
         }
     }
     return 4 * pi;
+    // Should be done and work
 }
 double compute_sqrt(double x){
     int i;
@@ -72,15 +73,46 @@ int display_prime(int n){
 }
 void process_scores(){
     char name[20];
-    char student_names[100];
+    char student_names[20][50];
     char quit[5] = "q";
     int score;
+    int student_score[20];
     int i = 0;
     printf("Please enter a students name and score (type q to quit)");
     scanf("%c, %d", name, score);
-    strcpy(student_names, name);
-    i = strlen(name);
-    
+    while (name = 'q' || 'Q'){
+        stpcpy(student_names[i], name);
+        student_score[i] = score;
+        i++;
+        printf("Please enter a students name and score (type q to quit)");
+        scanf("%c, %d", name, score);
+    }
+    int min = 0;
+    int max= 0;
+    double average = 0;
+    int sort;
+    int count = 0;
+
+    for(sort = 1; sort < i; sort++ ){
+        if(student_score[sort] > student_score[max]){
+            max = sort;
+        }
+    }
+    printf("Student with the max score is %c with %d\n", student_names[max + 1], student_score[max]);
+
+    for(sort = 1; sort < i; sort++ ){
+        if(student_score[sort] < student_score[min]){
+            min = sort;
+        }
+    }
+    printf("Student with the min score is %c with %d\n", student_names[min + 1], student_score[min]);
+
+    for(sort = 0; sort < i; sort++ ){
+        sort = student_score[count] + sort;
+        count++;
+    }
+    average = (sort/i);
+    printf("The average score was %f", average);
 }
 double compute_tax(int income, char *status, char state){
     double tax;
@@ -138,7 +170,7 @@ int quadratic(double a, double b, double c, double *solution1, double *solution2
 
 }
 int sum_squares(int n){
-
+    // needs to be reclusive
 }
 void file_count(char *file, int *characters, int *blanks, int *lines){
     FILE *fp;
@@ -168,7 +200,7 @@ void file_count(char *file, int *characters, int *blanks, int *lines){
 
 }
 void file_sort(char *infile, char *outfile){
-    char names[20][50];
+    char student_names[20][50];
     int student_id[50];
     double gpa[50];
     int count, i;
@@ -176,17 +208,17 @@ void file_sort(char *infile, char *outfile){
     char name [20];
     int blank;
     FILE *fp;
-    fp = fopen(*infile, "r");
-    count = fgetc(fp);
+    fp = fopen(*infile, "r"); // Open File
+    count = fgetc(fp); //count gets the number at the start of the page to know how many students their are
     while(fp != EOF){
         temp = fgetc(fp);
         if (temp == isalpha)
         {
-            strcpy(name, temp);
+            strcat(name, temp); //Copies characters to name to be help untile they are put in the array
         }
         if (temp == isblank)
         {
-            blank++;
+            blank++; //counting the blanks, 1 means id, 2 means gpa
         }
         
         if (temp == isdigit && blank == 1)
@@ -201,7 +233,12 @@ void file_sort(char *infile, char *outfile){
         {
             i++;
         }
+        blank = 0;
+        strcpy(student_names[i], name);
     }
+    // Need to sort the by id
+
+    // After sorting output the number of students then print each student's info in ascending order by studentID
 
 }
 void file_student(char *infile){
@@ -209,20 +246,21 @@ void file_student(char *infile){
 }
 int main(){
     int choice;
-    int bool;
     printf("Please choose which fuction you would like to use:\n1: Computing Pi\n2: Computing Square\n3: Displaying Primes\n4: Processing Grades\n5: Computing tax\n6: Solving Quadratic\n7: Computing Sum Square\n8: Counting Files\n9: Sorting Files\n10: Student Files\n11: Quit\n");
     scanf("%d",choice);
-    if (choice == 1)
-    {
-        int n;
+    while(choice != 1){
+        if (choice == 1){
+            int n;
             double sqr;
             int choice;
             printf("Enter in a number you would like to square");
             scanf("%d",n);
             sqr = compute_pi(n);
             printf("The pi of %d is; %f", n, sqr);
-    }else
-            printf("Thank you for using 'program.c' :)");
+        }
+
+    
+    }
 
 }
    
